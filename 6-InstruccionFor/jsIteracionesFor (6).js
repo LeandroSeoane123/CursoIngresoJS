1c7f297a780d256c8bb4fc17f9151e5c8d0fb698
LeandroSeoane123 <52429721+LeandroSeoane123@users.noreply.github.com>
2019-08-05 22:05:58 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
+var numero;
+var contador = 0;
+numero = parseInt(prompt("Ingrese un numero"));
 
+for(var i = 0; i < numero; i++){
+if(i %2 == 0){
+    console.log  (numero);
+}
+contador = contador + 1
+}
+console.log ("La cantidad de numeros es " + contador)
 
 
 
