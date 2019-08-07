599c6afa0e6ac14046801f8ab95518319bbcd55e
LeandroSeoane123 <52429721+LeandroSeoane123@users.noreply.github.com>
2019-08-07 16:34:40 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,63 @@
 function mostrar()
 {
+var hora;
 
+hora = document.getElementById("laHora").value;
+
+
+switch(hora){  
+case "6":
+alert("Es de mañana");
+break;
+case "7":
+alert("Es de mañana");
+break;
+case "8":
+alert("Es de mañana");
+break;
+case "9":
+alert("Es de mañana");
+break;
+case "10":
+alert("Es de mañana");
+break;
+case "11":
+alert("Es de mañana");
+break;
+case "12":
+alert("Es de tarde");
+break;
+case "13":
+alert("Es de tarde");
+break;
+case "14":
+alert("Es de tarde");
+break;
+case "15":
+alert("Es de tarde");
+break;
+case "16":
+alert("Es de tarde");
+break;
+case "17":
+alert("Es de tarde");
+break;
+case "18":
+alert("Es de tarde");
+break;
+case "19":
+alert("Es de tarde");
+break;
+default: if(hora > 24 || isNaN(hora)){
+    alert("La hora ingresada no es valida");
+    break;
+}
+else{
+    alert("Es de noche, a dormir");
+    break;
+}
+
+
+
+}
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+var precio;
+var descuento;
+var precioFinal;
+
+precio = prompt("Ingrese el precio.");
+
+parseInt(precio);
+
+descuento = prompt("Ingrese el descuento");
+
+parseInt(descuento);
+
+descuento = precio * descuento / 100;
+
+precioFinal = precio - descuento;
+
+document.getElementById("elPrecioFinal").value = precioFinal;
+
+
 
 }
